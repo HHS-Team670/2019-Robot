c3ecbd8ad7065c1f76d8b950c7fc84b74366e0b4
ShaylanDias <sdias695@student.fuhsd.org>
2018-12-28 19:10:58 -0800

    MustangPi for receiving doubles added. LED Control added

---
+++ 2019-Robot/src/main/java/frc/team670/robot/utils/TrajectoryConstructor.java
@@ -0,0 +1,16 @@
+package frc.team670.robot.utils;
+
+import edu.wpi.first.networktables.NetworkTable;
+
+public class TrajectoryConstructor {
+
+
+
+    public TrajectoryConstructor(String tableName, String entryName){
+        // NetworkTable.
+    }
+
+
+
+
+}
