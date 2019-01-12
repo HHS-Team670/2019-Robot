7913c637c83ad3ad8601872c85a21fc096d20e5f
Arnuv Tandon <43461672+arnuvtandon@users.noreply.github.com>
2019-01-12 15:19:50 -0800

    code that compiles

--- LED-Arduino/arduino/lightFromScratch.ino
+++ LED-Arduino/arduino/lightFromScratch.ino
@@ -9,8 +9,7 @@
                                                     //Imports:
 #include <SPI.h>                                    //SPI interface for interfacing with Ethernet Shield
 #include <Ethernet.h>                               //Ethernet library for creating a client instance
-#include <Adafruit_NeoPixel.h>                      //Adafruit's NeoPixel library for controlling NeoPixels
-
+#include <Adafruit_NeoPixel.h> 
 
 Adafruit_NeoPixel strip =                           //Defines an Adafruit Neopixel strip, containing 120 LEDs, using 
   Adafruit_NeoPixel(120, 6, NEO_GRB + NEO_KHZ800);  //Arduino pin #6, and using the GRB format at 800KHZ bitstream
@@ -19,30 +18,27 @@ EthernetClient robotClient;                         //Defines a client to be use
 byte mac[] = {                                      //Creates a mac address for use in defining an Ethernet instance
   0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
 };
-I
-PAddress ip(10,9,57,6);                            //Defines a static IP for the Arduino/Ethernet Shield
+IPAddress ip(10,9,57,6);                            //Defines a static IP for the Arduino/Ethernet Shield
 IPAddress robotIp(10,9,57,2);                       //Defines the robot's IP
 
 int connectionTimer = 0;                            //Sets a connection timer to see if the program should reconnect to the RoboRio in case it becomes disconnected
 
 char dataChar;                                      //Used for storing a character before inputing it into dataArray[]
 String dataString = "";                             //Used for building a string to then splice into multiple parts to control the LEDs
-String alliance = "invalid";                        //Sets a default alliance,
 String xFinal = "2";                                //data for where the peg is via vision program,
 String truexFinal = "center";
 String outake = "succesful";                        //If the robot is in driver controlled mode
-const int numberOfPixels=strip.numPixels;
+
 
 
 String alliance = "Invalid";
 String climbing = "false  ";
-String forwardDrive="false  "
-String reverseDrive-="false  ";
+String forwardDrive="false  ";
+String reverseDrive="false  ";
 String still="false  ";
 String visionLock="false  ";
-String data = "Invalid,false,false,"
-int xFinal = 2;
-
+String data = "Invalid,false,false,";
+int numberOfPixels=strip.numPixels();
 
 int asciiArray[] = {                                //Please use the ASCII converter at https://www.arduino.cc/en/Reference/ASCIIchart
     32,44,45,48,49,50,51,52,53,54,55,56,            //to translate ASCII and add more possible characters to be readable
@@ -59,7 +55,6 @@ int r;                                              //Alliance color red value
 int b;                                              //Alliance color blue value
 
 
-robotState robotState = still;
   
 void setup() {                                      //Sets up constants before program begins     
   Ethernet.begin(mac,ip);                           //Initalizes an Ethernet instance
@@ -68,7 +63,8 @@ void setup() {                                      //Sets up constants before p
   strip.begin();                                    //Starts communication with the NeoPixel strip
 }
 
-void loop() {                                       //Ran indefinitly after setup()
+void loop() {
+                                         //Ran indefinitly after setup()
   connectionTimer++;                                //Adds a count to the ConnectionTimer
   
   if(robotClient.available()){                      //Runs when bytes are available to read
@@ -100,74 +96,107 @@ void loop() {                                       //Ran indefinitly after setu
     if(xFinal.equals("3")){                              //If xFinal is equal to 3, it is to the right of the camera
       truexFinal = "right";
     }
-   //				data = alliance + "," + climbing + "," + forwardDrive+ "," +reverseDrive + "," + still+ "," +visionLock+","+xFinal;
 
     Serial.println("Alliance?: " + alliance +       //Prints out the data above to the serial console
       ", Climbing?: "  + climbing + ", forwardDrive?: " + 
-      forwardDrive + ", reverseDrive?: " + reverseDrive + ", still?:" still+", visionLock?:"+visionLock+", xFinal?:"+xFinal); 
+      forwardDrive + ", reverseDrive?: " + reverseDrive + ", still?:" +still+", visionLock?:"+visionLock+", xFinal?:"+xFinal); 
+  
   }
-      
+
   //Below here is code to control the LED's from the data obtained above
   
-  for(int i = 0; i < strip.numPixels(); i++){       //Resets the full LED strip...
+  for(int i = 0; i < numberOfPixels; i++){       //Resets the full LED strip...
     strip.setPixelColor(i,0,0,0);                   //...by setting each LED to black
   }
-  void reset(){
-  strip.begin();
-  strip.show(); //initialize all pixels to "off"
-  
+//  void reset(){
+//  strip.begin();
+//  strip.show(); //initialize all pixels to "off"
+//  }
+ if(alliance == "blue   "){                        //If the alliance is blue, set the base LED color to blue
+    r = 0;
+    b = 255;
   }
-  //light show for climbing
-  void climbing() { 
-    while(climbing.equals("climbing")){
-      for(int i = 0; i < numberOfPixels; i++;){
-        trip.setPixelColor(i,76,156,20);
+  if(alliance == "red    "){                        //If the alliance is red, set the base LED color to red
+    r = 255;
+    b = 0;
+  }
+  if(alliance == "invalid"){                        //If no alliance is specified, set the base LED color to purple
+    r = 255;
+    b = 255;
+  }
+  
+ 
+if(climbing.equals("climbing")){
+    while(climbing.equals("climbing"))
+    {
+      for(int i = 0; i < numberOfPixels; i++){
+        strip.setPixelColor(i,76,156,20);
         strip.show();
     }
-          strip.reset();//allows strip to reset and display LEDs again – allows the LEDs to "snake around the strip"
+         //strip.reset();//allows strip to reset and display LEDs again – allows the LEDs to "snake around the strip"
   }
 }
+//}
 //light show for vision lock
-  void visionLock(){
+  if(visionLock.equals("visionlocked")){
     while(visionLock.equals("visionLocked")){
-      for(int i = 0; i < numberOfPixels; i++;){
+      for(int i = 0; i < numberOfPixels; i++){
         strip.setPixelColor(i,r,b,0);
     }
           strip.show();
   }
-  
+
 }
 //light show for forward drive
-  void forwardDrive(){
+  if(forwardDrive.equals("forwardDrive")){
     while(forwardDrive.equals("forwardDrive")){
       for(int i = 0; i < numberOfPixels; i++){
-        strip.setPixelColor(i,200,200,0)
+        strip.setPixelColor(i,200,200,0);
     }
           strip.show();
   }
 }
 //light show for reverse drive 
-  void reverseDrive(){
+  if(reverseDrive.equals("reverseDrive")){
     while(reverseDrive.equals("reverseDrive")) {
       for(int i = 0; i < numberOfPixels; i++)
       {
-        strip.setPixelColor(i,200,200,200)
+        strip.setPixelColor(i,200,200,200);
     }
           strip.show();
   }
 }
 
-  void  stillDrive(){
-   while(still.equals("true"))
+  if(still.equals("still")){
+   while(still.equals("true")){
       for(int i = 0; i < numberOfPixels; i++)
       {
-       strip.setPixelColor(i,200,200,200)
+       strip.setPixelColor(i,200,200,200);
+    }
+   }
+  }
+  if(connectionTimer > 20){                         //About 1 second has passed since the last packet when one should come in every 1/4 of a second
+    connectionTimer = 0;                            //Resets the timer
+    robotClient.stop();                             //Forces a socket disconnect from the RoboRio
+    robotClient.connect(robotIp, 5801);             //Re-initalizes socket communication with the Rio
+  }
+}
+char asciiConvert(EthernetClient client){           //Changes an ASCII byte from a socket to a string
+
+  int data = client.read();                         //Reads the socket for data from server
+  int dataPoint = -666;                             //Defaults the point that the recived bytes equal a char in asciiArray[]
+  for(int i=0; i < 37 -1; i++){                     //Runs the length of asciiArray[]
+    if(asciiArray[i] == data){                      //If the data recived equals a point in asciiArray[]...
+      dataPoint = i;                                //...the program sets dataPoint to the point of equality
+    }
+  }
+
+  char finalData = translationArray[dataPoint];     //Sets the final character 
+  if(dataPoint != -666){                            //If the data point is not null...
+    return finalData;                               //Returns the transcribed ASCII character
+  }else{
+    char defaultchar[] = {"E"};                     //Sets a default char if there is an error
+    return defaultchar[0];                          //Returns the default error char
+  }
 }
 
-  while(true){
-    climbing();
-    visionLock();
-    forwardDrive();
-    reverseDrive();
-    stillDrive();
-}
