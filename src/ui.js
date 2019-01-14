58c0048adecf31bb58a8341e912bdb6d2bb807ae
kishores4200 <kishores4200@gmail.com>
2019-01-13 20:31:52 -0800

    finished field map

--- src/ui.js
+++ src/ui.js
@@ -13,7 +13,7 @@ var ui = {
     }
 };
 
-let allFieldLocations = ["left-rocket-top", "left-rocket-middle", "left-rocket-bottom", "left-cargo-far", "left-cargo-mid", "left-cargo-close", "left-cargo-alliance", "right-cargo-alliance", "right-cargo-far", "right-cargo-mid", "right-cargo-close", "right-rocket-top", "right-rocket-middle", "right-rocket-bottom", "right-loading-station", "left-loading-station"];
+let allFieldLocations = ["left-rocket-top", "left-rocket-middle", "left-rocket-bottom", "left-cargo-far", "left-cargo-mid", "left-cargo-close", "left-cargo-alliance", "right-cargo-alliance", "right-cargo-far", "right-cargo-mid", "right-cargo-close", "right-rocket-top", "right-rocket-middle", "right-rocket-bottom", "left-loading-cargo", "left-loading-plate", "right-loading-cargo", "right-loading-plate"];
 
 NetworkTables.addKeyListener('/SmartDashboard/robotTime', (key, value) => {
   var minutes = ~~(value / 60); // converts to integer
@@ -43,71 +43,56 @@ window.onkeyup = function(event) {
     if (key == '9') {
       keys = [];
     } else if (key == '0') {
-      keys.pop();
+      var toRemove = keys.pop();
+      highlight(getMap(toRemove), 'clear');
+    } else if (key == 'w') {
+      finalize();
+      keys = [];
     } else {
-      keys.push(key);
+      if (keys.length == 0 && getMap(key) != 'unmapped') clearHighlight();
+      if (keys.length < 2) {
+        if (getMap(key) != 'unmapped') keys.push(key);
+        highlight(getMap(key), 'blue');
+      }
     }
 
     document.getElementById('test').innerHTML = keys;
-
-
-    // if (key == 'a') {
-    //   highlight('left-rocket-top');
-    // }
-    // if (key == 'j') {
-    //   highlight('left-rocket-middle');
-    // }
-    // if (key == 's') {
-    //   highlight('left-rocket-bottom');
-    // }
-    // if (key == 'b') {
-    //   highlight('left-cargo-far');
-    // }
-    // if (key == 'k') {
-    //   highlight('left-cargo-mid');
-    // }
-    // if (key == 't') {
-    //   highlight('left-cargo-close');
-    // }
-    // if (key == '2') {
-    //   highlight('left-cargo-alliance');
-    // }
-    // if (key == '3') {
-    //   highlight('right-cargo-alliance');
-    // }
-    // if (key == 'c') {
-    //   highlight('right-cargo-far');
-    // }
-    // if (key == 'l') {
-    //   highlight('right-cargo-mid');
-    // }
-    // if (key == 'u') {
-    //   highlight('right-cargo-close');
-    // }
-    // if (key == 'd') {
-    //   highlight('right-rocket-top');
-    // }
-    // if (key == 'm') {
-    //   highlight('right-rocket-middle');
-    // }
-    // if (key == 'v') {
-    //   highlight('right-rocket-bottom');
-    // }
-    // if (key == '1') {
-    //   highlight('left-loading-station');
-    // }
-    // if (key == '4') {
-    //   highlight('right-loading-station');
-    // }
-    //
-    // if (key == '9') {
-    //   clearHighlight();
-    // }
 }
 
-function highlight(toHighlight) {
+// returns the value the key is mapped to
+function getMap(key) {
+  if (key == 'a') return 'left-rocket-top';
+  if (key == 'j') return 'left-rocket-middle';
+  if (key == 's') return 'left-rocket-bottom';
+  if (key == 'b') return 'left-cargo-far';
+  if (key == 'k') return 'left-cargo-mid';
+  if (key == 't') return 'left-cargo-close';
+  if (key == '2') return 'left-cargo-alliance';
+  if (key == '3') return 'right-cargo-alliance';
+  if (key == 'c') return 'right-cargo-far';
+  if (key == 'l') return 'right-cargo-mid';
+  if (key == 'u') return 'right-cargo-close';
+  if (key == 'd') return 'right-rocket-top';
+  if (key == 'm') return 'right-rocket-middle';
+  if (key == 'v') return 'right-rocket-bottom';
+  if (key == '!') return 'left-loading-cargo';
+  if (key == '1') return 'left-loading-plate';
+  if (key == '$') return 'right-loading-cargo';
+  if (key == '4') return 'right-loading-plate';
+
+  if (key == '9') return 'clear';
+  if (key == '0') return 'backspace';
+  if (key == 'w') return 'enter';
+
+  return 'unmapped';
+}
+
+function highlight(toHighlight, color) {
   var location = document.getElementById(toHighlight);
-  location.style.fill = `rgb(0,255,0)`;
+  if (color == 'green') location.style.fill = `rgb(0,255,0)`;
+  if (color == 'red') location.style.fill = `rgb(255,0,0)`;
+  if (color == 'blue') location.style.fill = `rgb(0,0,255)`;
+  if (color == 'clear') location.style.fill = `rgb(0,0,0)`;
 }
 
 function clearHighlight() {
@@ -116,4 +101,26 @@ function clearHighlight() {
     var location = allFieldLocations[i];
     document.getElementById(location).style.fill = `rgb(0,0,0)`;
   }
+}
+
+function finalize() {
+  highlight(getMap(keys[0]), 'green');
+  highlight(getMap(keys[1]), 'red');
+
+  var intakeStart = 'plate';
+  if (getMap(keys[0]).includes('cargo')) {
+    intakeStart = 'cargo';
+  }
+
+  var intakeEnd = 'bottom';
+  if (getMap(keys[1]).includes('top')) {
+    intakeEnd = 'top';
+  } else if (getMap(keys[1]).includes('middle')) {
+    intakeEnd = 'middle';
+  }
+
+  NetworkTables.putValue('/SmartDashboard/path/robot-start', keys[0]);
+  NetworkTables.putValue('/SmartDashboard/path/robot-end', keys[1]);
+  NetworkTables.putValue('/SmartDashboard/path/intake-start', intakeStart);
+  NetworkTables.putValue('/SmartDashboard/path/intake-end', intakeEnd);
 }
