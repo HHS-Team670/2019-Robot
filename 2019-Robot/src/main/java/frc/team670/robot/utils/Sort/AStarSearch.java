2cf6338a8bbdf092d4c0d3a44b72e1be54fbe962
Rohit Ghosh <rohitghosh@Nikhils-MacBook-Air.local>
2019-01-18 17:21:15 -0800

    Made big changes

--- 2019-Robot/src/main/java/frc/team670/robot/utils/Sort/AStarSearch.java
+++ 2019-Robot/src/main/java/frc/team670/robot/utils/Sort/AStarSearch.java
@@ -2,27 +2,52 @@ package frc.team670.robot.utils.sort;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Queue;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 /**
- * Implementation of an A* search algorithm
+ * Implementation oxf an A* search algorithm
  * https://www.geeksforgeeks.org/a-search-algorithm/
+ * 
  * @author ctchen, laksh, rghosh670
  */
 public class AStarSearch {
     /**
      * Runs the search function
-     * @param start The node that the user wishes to start from
+     * 
+     * @param start       The node that the user wishes to start from
      * @param destination The target node that the use wishes to reach
      * @return A path of nodes that the search algorithm has found
      */
-    public static List<Node> search(Node start, Node destination){
+    public static List<Edge> search(Node start, Node destination) {
 
-        Queue<Node> openList = new LinkedList<Node>();
+        HashMap<Node, Edge> cameFrom = new HashMap<Node, Edge>();
+        Map<Node, Integer> fValues = new HashMap<Node, Integer>();
+        Map<Node, Integer> gValues = new HashMap<Node, Integer>();
+
+        Comparator<Node> fValueComp = new Comparator<Node>() {
+            @Override
+
+
+            public int compare(Node o1, Node o2) {
+                int o1f = Integer.MAX_VALUE;
+                int o2f = Integer.MAX_VALUE;
+
+                if (fValues.get(o1) != null)
+                    o1f = fValues.get(o1);
+
+                if (fValues.get(o2) != null)
+                    o2f = fValues.get(o2);
+
+                return fValues.get(o1).compareTo(fValues.get(o2));
+            }
+        };
+
+        SortedSet<Node> openList = new TreeSet<Node>(fValueComp);
         List<Node> closedList = new ArrayList<Node>();
 
         Node current = null;
@@ -30,72 +55,57 @@ public class AStarSearch {
 
         boolean end = false;
 
-        HashMap<Node, Edge> cameFrom = new HashMap<Node, Edge>();
-        Map<Node, Integer> fValues = new HashMap<Node, Integer>();
-        Map<Node, Integer> gValues = new HashMap<Node, Integer>();
         gValues.put(start, 0);
 
-        while(!openList.isEmpty() && !end){
-            current = openList.poll();
-            closedList.add(current);
-           
-            if(current.equals(destination)){
+        fValues.put(start, start.getHeuristicDistance(destination));
+
+        while (!openList.isEmpty() && !end) {
+
+            current = openList.first();
+
+            if (current.equals(destination)) {
                 end = true;
             }
 
-            for(Edge e : current.getEdges()){
+            openList.remove(current);
+            closedList.add(current);
+
+            for (Edge e : current.getEdges()) {
                 Node child = e.getDest();
+
+                if (closedList.contains(child)) {
+                    continue;
+                }
+
                 int tempG = gValues.get(current) + e.getCost();
                 int tempF = tempG + child.getHeuristicDistance(destination);
 
-                 if(closedList.contains(child) && (fValues.get(child) == null || tempF >= fValues.get(child))) {
-                        continue;
-                  } else if(!openList.contains(child) || (tempF < deal with fvalues child.getF())){
-                    cameFrom.put(child, e);
-
-                    gValues.set
-                    fValues.set 
-                    child.setG(tempG);
-                    child.setF(tempF);
-
-                    if(openList.contains(child)){
-                        openList.remove(child);
-                    }
-
+                if (!openList.contains(child) || fValues.get(child) == null) {
+                    fValues.put(child, tempF);
                     openList.add(child);
-                  }
-    
+                } else if (gValues.get(child) == null || tempG >= gValues.get(child)) {
+                    continue;
+                }
+                cameFrom.put(child, e);
+                gValues.put(child, tempG);
             }
 
-            
         }
-        return getPath(destination);
+
+        return getPath(start, destination, cameFrom);
     }
 
+    private static List<Edge> getPath(Node start, Node destination, HashMap<Node, Edge> cameFrom) {
+        List<Edge> path = new ArrayList<Edge>();
+        Node node = destination;
 
-    private static List<Node> getPath(Node destination, Map<Node, Node> cameFrom){
-        List<Node> path = new ArrayList<Node>();
-
-            for(Node node = destination; node != null; node = node.getParent()){
-                path.add(node);
-            }
+        while (!node.equals(start)) {
+            Edge e = cameFrom.get(node);
+            path.add(e);
+            node = e.getSource();
+        }
 
         Collections.reverse(path);
         return path;
     }
-
-    private static double getDist(Node d1, Node d2){
-        double d1X = d1.getCoord().getX();
-        double d1Y = d1.getCoord().getX();
-        double d2X = d2.getCoord().getX();
-        double d2Y = d2.getCoord().getX();
-
-        return Math.sqrt((d2Y - d1Y) * (d2Y - d1Y) + (d2X - d1X) * (d2X - d1X));
-    }
-
-    private static double calcH(Node node, Node destination){
-        return getDist(node, destination);
-    }
-    
 }
-
