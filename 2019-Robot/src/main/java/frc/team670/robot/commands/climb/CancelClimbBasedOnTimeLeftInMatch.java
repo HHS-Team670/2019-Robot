52727ebff32089583d993762c4fc6fdf2f995c2b
Rohit Ghosh <rohitghosh@Nikhils-MacBook-Air.local>
2019-01-27 16:42:37 -0800

    Update CancelClimbBasedOnTimeLeftInMatch.java

--- 2019-Robot/src/main/java/frc/team670/robot/commands/climb/CancelClimbBasedOnTimeLeftInMatch.java
+++ 2019-Robot/src/main/java/frc/team670/robot/commands/climb/CancelClimbBasedOnTimeLeftInMatch.java
@@ -14,12 +14,17 @@ import frc.team670.robot.commands.climb.pistonClimb.AbortRobotPistonClimb;
 import frc.team670.robot.subsystems.Arm;
 import frc.team670.robot.subsystems.Climber;
 
+/**
+ * I assume that this would be running constantly and once a small amount of time is left in the match
+ * and the front pistons haven't been retracted (in which case we assume the drivers should just get onto the platform instead),
+ * it adds a command to stow the arm and bring the pistons down to flat
+ */
 public class CancelClimbBasedOnTimeLeftInMatch extends InstantCommand {
   private Arm arm;
   private Climber climber;
   private DriverStation ds;
 
-  private static final int MIN_TIME_FOR_CLIMB = 5;
+  private static final int MIN_TIME_FOR_CLIMB = 0; //TODO set this based on testing the climber itself and seeing how long it takes drivers to climb
 
   public CancelClimbBasedOnTimeLeftInMatch(Arm arm, Climber climber) {
     this.climber = climber;
