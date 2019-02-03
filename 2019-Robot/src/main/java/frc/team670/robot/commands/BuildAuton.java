63b2b3ba490d85649164bcd83248afd29d920200
kishores4200 <kishores4200@gmail.com>
2019-02-03 14:32:29 -0800

    modified auton builder

--- 2019-Robot/src/main/java/frc/team670/robot/commands/BuildAuton.java
+++ 2019-Robot/src/main/java/frc/team670/robot/commands/BuildAuton.java
@@ -8,10 +8,10 @@
 package frc.team670.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-import frc.team670.robot.commands.arm.BuildArmSequence;
 import frc.team670.robot.commands.arm.movement.MoveArm;
 import frc.team670.robot.commands.arm.movement.PlaceOrGrab;
 import frc.team670.robot.commands.drive.DriveMotionProfile;
+import frc.team670.robot.commands.drive.vision.AdvancedVisionPIDDrive;
 import frc.team670.robot.subsystems.Arm;
 import frc.team670.robot.subsystems.Arm.LegalState;
 
@@ -20,7 +20,7 @@ public class BuildAuton extends CommandGroup {
   /**
    * Add your docs here.
    */
-  public BuildAuton(String[] autonSequence, Arm arm, boolean isReversed) {
+  public BuildAuton(String[] autonSequence, Arm arm, boolean isRobotFacingBack) {
     String start = autonSequence[0];
     String target1 = autonSequence[1];
     String height1 = autonSequence[2];
@@ -31,50 +31,80 @@ public class BuildAuton extends CommandGroup {
     LegalState destination; 
     String fileName = "";
 
-    destination = getLegalState(target1, height1); 
+    destination = getLegalState(target1, height1, isRobotFacingBack); 
     fileName = start + "_" + target1 + ".pf1.csv";
-    addSequential(new DriveMotionProfile(fileName, isReversed));
-    addParallel(new BuildArmSequence(new String[] {destination.toString(), "place"}, arm));
-    // addSequential: turn to proper angle to start next path?
+    addSequential(new DriveMotionProfile(fileName, isRobotFacingBack));
+    addParallel(new MoveArm(Arm.getArmState(destination), arm));
+    addSequential(new AdvancedVisionPIDDrive());
+    if (destination != LegalState.NEUTRAL) addSequential(new PlaceOrGrab(true));
  
-    destination = getLegalState(target2, height2); 
+    destination = getLegalState(target2, height2, isRobotFacingBack); 
     fileName = target1 + "_" + target2 + ".pf1.csv";
-    addSequential(new DriveMotionProfile(fileName, isReversed));
-    addParallel(new BuildArmSequence(new String[] {destination.toString(), "grab"}, arm));
-    // addSequential: turn to proper angle to start next path?
+    addSequential(new DriveMotionProfile(fileName, !isRobotFacingBack));
+    addParallel(new MoveArm(Arm.getArmState(destination), arm));
+    addSequential(new AdvancedVisionPIDDrive());
+    if (destination != LegalState.NEUTRAL) addSequential(new PlaceOrGrab(false));
 
-    destination = getLegalState(target3, height3); 
+    destination = getLegalState(target3, height3, isRobotFacingBack); 
     fileName = target2 + "_" + target3 + ".pf1.csv";
-    addSequential(new DriveMotionProfile(fileName, isReversed));
-    addParallel(new BuildArmSequence(new String[] {destination.toString(), "place"}, arm));
+    addSequential(new DriveMotionProfile(fileName, isRobotFacingBack));
+    addParallel(new MoveArm(Arm.getArmState(destination), arm));
+    addSequential(new AdvancedVisionPIDDrive());
+    if (destination != LegalState.NEUTRAL) addSequential(new PlaceOrGrab(true));
   }
 
-  // TODO account for direction (FRONT or BACK)
-  private LegalState getLegalState(String target, String height) {
-    if (target.contains("Rocket") && (target.contains("1") || target.contains("3"))) {
-        if (height.equals("MIDDLE")) {
-            return LegalState.PLACE_HATCH_ROCKET_MIDDLE_BACK;
-        } else if (height.equals("LOW")) {
-            return LegalState.LOW_HATCH_BACK;
+  private LegalState getLegalState(String target, String height, boolean isFacingBack) {
+    if (isFacingBack) {
+        if (target.contains("Rocket") && (target.contains("1") || target.contains("3"))) {
+            if (height.equals("MIDDLE")) {
+                return LegalState.PLACE_HATCH_ROCKET_MIDDLE_BACK;
+            } else if (height.equals("LOW")) {
+                return LegalState.LOW_HATCH_BACK;
+            }
+        }
+        else if (target.contains("Rocket") && target.contains("2")) {
+            if (height.equals("MIDDLE")) {
+                return LegalState.PLACE_BALL_ROCKET_MIDDLE_BACK;
+            } else if (height.equals("LOW")) {
+                return LegalState.PLACE_BALL_ROCKET_LOW_BACK;
+            }
+        }
+        else if (target.contains("Cargo")) {
+            // if placing ball
+            if (height.equals("MIDDLE")) {
+                return LegalState.PLACE_BALL_CARGOSHIP_BACK;
+            } 
+            // if placing hatch
+            else if (height.equals("LOW")) {
+                return LegalState.LOW_HATCH_BACK;
+            }
+        }
+    } else {
+        if (target.contains("Rocket") && (target.contains("1") || target.contains("3"))) {
+            if (height.equals("MIDDLE")) {
+                return LegalState.PLACE_HATCH_ROCKET_MIDDLE_FORWARD;
+            } else if (height.equals("LOW")) {
+                return LegalState.LOW_HATCH_FORWARD;
+            }
+        }
+        else if (target.contains("Rocket") && target.contains("2")) {
+            if (height.equals("MIDDLE")) {
+                return LegalState.PLACE_BALL_ROCKET_MIDDLE_FORWARD;
+            } else if (height.equals("LOW")) {
+                return LegalState.PLACE_BALL_ROCKET_LOW_FORWARD;
+            }
+        }
+        else if (target.contains("Cargo")) {
+            // if placing ball
+            if (height.equals("MIDDLE")) {
+                return LegalState.PLACE_BALL_CARGOSHIP_FORWARD;
+            } 
+            // if placing hatch
+            else if (height.equals("LOW")) {
+                return LegalState.LOW_HATCH_FORWARD;
+            }
         }
     }
-    else if (target.contains("Rocket") && target.contains("2")) {
-        if (height.equals("MIDDLE")) {
-            return LegalState.PLACE_BALL_ROCKET_MIDDLE_BACK;
-        } else if (height.equals("LOW")) {
-            return LegalState.PLACE_BALL_ROCKET_LOW_BACK;
-        }
-    }
-    else if (target.contains("Cargo")) {
-        // if placing ball
-        if (height.equals("MIDDLE")) {
-            return LegalState.PLACE_BALL_CARGOSHIP_BACK;
-        } 
-        // if placing hatch
-        else if (height.equals("LOW")) {
-            return LegalState.LOW_HATCH_BACK;
-        }
-    }
-    return null;
+    return LegalState.NEUTRAL;
   }
 }
