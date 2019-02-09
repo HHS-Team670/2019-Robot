c0958d0f1d4bb6d6056c915907f2e45c4dd9a5d0
ShaylanDias <shaylandias@gmail.com>
2019-02-08 20:44:45 -0800

    Fixed ArmTransitionTest

--- 2019-Robot/src/test/java/frc/team670/robot/commands/arm/movement/TestElbow.java
+++ 2019-Robot/src/test/java/frc/team670/robot/commands/arm/movement/TestElbow.java
@@ -7,8 +7,6 @@
 
 package frc.team670.robot.commands.arm.movement;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
 import frc.team670.robot.subsystems.elbow.BaseElbow;
 
 /**
@@ -19,8 +17,8 @@ public class TestElbow extends BaseElbow {
 
     private double elbowAngle;
 
-    public TestElbow(TalonSRX rotatorTalon, double arbitrary_feedforward_constant, int forward_soft_limit, int reverse_soft_limit, boolean timeout, int QUAD_ENCODER_MIN, int QUAD_ENCODER_MAX, int CONTINUOUS_CURRENT_LIMIT, int PEAK_CURRENT_LIMIT){
-        super(rotatorTalon, arbitrary_feedforward_constant, forward_soft_limit, reverse_soft_limit, timeout, QUAD_ENCODER_MIN, QUAD_ENCODER_MAX, CONTINUOUS_CURRENT_LIMIT, PEAK_CURRENT_LIMIT);
+    public TestElbow(){
+        super(null, 0.0, 0, 0, false, 0, 0, 0, 0);
     }
 
     @Override
