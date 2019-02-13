ba476a0c9ff8e16e0a155372f6c5e5f43d7ccaef
ShaylanDias <shaylandias@gmail.com>
2019-02-12 20:46:09 -0800

    Flip Camera

--- 2019-Robot/src/main/java/frc/team670/robot/commands/cameras/FlipCamera.java
+++ 2019-Robot/src/main/java/frc/team670/robot/commands/cameras/FlipCamera.java
@@ -2,11 +2,17 @@ package frc.team670.robot.commands.cameras;
 
 import edu.wpi.first.wpilibj.command.InstantCommand;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.team670.robot.Robot;
 
 /**
  *  Flips the camera: The front camera or the camera on the back
  */
 public class FlipCamera extends InstantCommand{
+
+    /**
+     * Driver camera direction (true = front, false = back). Starts false because robot starts backwards.
+     */
+    private static boolean cameraDirection = false;
 
     public FlipCamera(){
         super();
@@ -15,6 +21,19 @@ public class FlipCamera extends InstantCommand{
     // called once when the command executes
     @Override
     protected void initialize(){
+        flipCameraDirection();
+        Robot.oi.rumbleDriverController(0.4, 0.1);
+    }
+
+    public static void flipCameraDirection() {
         SmartDashboard.putString("camera-source", "next");
+        cameraDirection = !cameraDirection;
+    }
+
+    /**
+     * Driver camera direction (true = front, false = back). Starts false because robot starts backwards.
+     */
+    public static boolean getCameraDirection() {
+        return cameraDirection;
     }
 }
