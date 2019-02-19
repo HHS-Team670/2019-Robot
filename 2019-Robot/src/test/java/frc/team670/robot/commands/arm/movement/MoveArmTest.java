29a0d199ba9dd6231b7cbc249b4d4a109046bbd0
ShaylanDias <shaylandias@gmail.com>
2019-02-18 21:06:42 -0800

    Start Position

--- 2019-Robot/src/test/java/frc/team670/robot/commands/arm/movement/MoveArmTest.java
+++ 2019-Robot/src/test/java/frc/team670/robot/commands/arm/movement/MoveArmTest.java
@@ -18,6 +18,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.team670.robot.subsystems.Arm;
 import frc.team670.robot.subsystems.Arm.ArmState;
+import frc.team670.robot.subsystems.Arm.LegalState;
 import frc.team670.robot.subsystems.Claw;
 import frc.team670.robot.subsystems.Intake;
 import frc.team670.robot.utils.functions.MathUtils;
@@ -40,80 +41,84 @@ public class MoveArmTest {
         for(ArmState state : Arm.getStates().values()) {
             for(ArmState dest : Arm.getStates().values()) {
 
-                Command moveArmToStart = new MoveArmDangerous(state, arm, intake);
-                Scheduler.getInstance().add(moveArmToStart);  
-                moveArmToStart.setRunWhenDisabled(true); // Must be true or it won't run       
-                moveArmToStart.start();   
-                while(!moveArmToStart.isCompleted()) {
-                    Scheduler.getInstance().run();
-                }
+                if(!dest.equals(Arm.getArmState(LegalState.START))) {
+                    Command moveArmToStart = new MoveArmDangerous(state, arm, intake);
+                    Scheduler.getInstance().add(moveArmToStart);  
+                    moveArmToStart.setRunWhenDisabled(true); // Must be true or it won't run       
+                    moveArmToStart.start();   
+                    while(!moveArmToStart.isCompleted()) {
+                        Scheduler.getInstance().run();
+                    }
 
-                CommandGroup moveArm = ArmPathGenerator.getPath(dest, arm);
+                    CommandGroup moveArm = ArmPathGenerator.getPath(dest, arm);
 
-                Scheduler.getInstance().add(moveArm);  
-                moveArm.setRunWhenDisabled(true); // Must be true or it won't run       
-                moveArm.start();   
-                while(!moveArm.isCompleted()) {
-                    Scheduler.getInstance().run();
+                    Scheduler.getInstance().add(moveArm);  
+                    moveArm.setRunWhenDisabled(true); // Must be true or it won't run       
+                    moveArm.start();   
+                    while(!moveArm.isCompleted()) {
+                        Scheduler.getInstance().run();
+                    }
+            
+                    Point2D.Double armCoord = Arm.getCoordPosition(elbow.getAngleInDegrees(), wrist.getAngleInDegrees(), extension.getLengthInches());
+                    assertEquals(dest, Arm.getCurrentState());
+                    try {
+                        // System.out.println("start: " + state.getClass().getName() + ", dest: " + dest.getClass().getName());
+                        assertEquals(dest.getCoordPosition().x, armCoord.x, 0.5);
+                        assertEquals(dest.getCoordPosition().y, armCoord.y, 0.5); 
+                    }
+                    catch(OutOfMemoryError e) {
+                        throw new AssertionError("Start: " + state.getClass().getName() + ", Dest: " + dest.getClass().getName());
+                    }
+                    double expectedIntakeAngle;
+                    if(dest.isIntakeDeployed()) {
+                        expectedIntakeAngle = Intake.INTAKE_ANGLE_DEPLOYED;
+                    } else {
+                        expectedIntakeAngle = Intake.INTAKE_ANGLE_IN;
+                    }
+                    assertEquals(true, MathUtils.isWithinTolerance(expectedIntakeAngle, intake.getAngleInDegrees(), 0.5));
                 }
-        
-                Point2D.Double armCoord = Arm.getCoordPosition(elbow.getAngleInDegrees(), wrist.getAngleInDegrees(), extension.getLengthInches());
-                assertEquals(dest, Arm.getCurrentState());
-                try {
-                    // System.out.println("start: " + state.getClass().getName() + ", dest: " + dest.getClass().getName());
-                    assertEquals(dest.getCoordPosition().x, armCoord.x, 0.5);
-                    assertEquals(dest.getCoordPosition().y, armCoord.y, 0.5); 
-                }
-                catch(OutOfMemoryError e) {
-                    throw new AssertionError("Start: " + state.getClass().getName() + ", Dest: " + dest.getClass().getName());
-                }
-                double expectedIntakeAngle;
-                if(dest.isIntakeDeployed()) {
-                    expectedIntakeAngle = Intake.INTAKE_ANGLE_DEPLOYED;
-                } else {
-                    expectedIntakeAngle = Intake.INTAKE_ANGLE_IN;
-                }
-                assertEquals(true, MathUtils.isWithinTolerance(expectedIntakeAngle, intake.getAngleInDegrees(), 0.5));
             }
         }
 
         for(ArmState state : Arm.getStates().values()) {
             for(ArmState dest : Arm.getStates().values()) {
 
-                Command moveArmToStart = new MoveArmDangerous(state, arm, intake);
-                Scheduler.getInstance().add(moveArmToStart);  
-                moveArmToStart.setRunWhenDisabled(true); // Must be true or it won't run       
-                moveArmToStart.start();   
-                while(!moveArmToStart.isCompleted()) {
-                    Scheduler.getInstance().run();
-                }
+                if(!dest.equals(Arm.getArmState(LegalState.START))) {
+                    Command moveArmToStart = new MoveArmDangerous(state, arm, intake);
+                    Scheduler.getInstance().add(moveArmToStart);  
+                    moveArmToStart.setRunWhenDisabled(true); // Must be true or it won't run       
+                    moveArmToStart.start();   
+                    while(!moveArmToStart.isCompleted()) {
+                        Scheduler.getInstance().run();
+                    }
 
-                CommandGroup moveArm = ArmPathGenerator.getPath(dest, arm);
+                    CommandGroup moveArm = ArmPathGenerator.getPath(dest, arm);
 
-                Scheduler.getInstance().add(moveArm);  
-                moveArm.setRunWhenDisabled(true); // Must be true or it won't run       
-                moveArm.start();   
-                while(!moveArm.isCompleted()) {
-                    Scheduler.getInstance().run();
+                    Scheduler.getInstance().add(moveArm);  
+                    moveArm.setRunWhenDisabled(true); // Must be true or it won't run       
+                    moveArm.start();   
+                    while(!moveArm.isCompleted()) {
+                        Scheduler.getInstance().run();
+                    }
+            
+                    Point2D.Double armCoord = Arm.getCoordPosition(elbow.getAngleInDegrees(), wrist.getAngleInDegrees(), extension.getLengthInches());
+                    assertEquals(dest, Arm.getCurrentState());
+                    try {
+                        // System.out.println("start: " + state.getClass().getName() + ", dest: " + dest.getClass().getName());
+                        assertEquals(dest.getCoordPosition().x, armCoord.x, 0.5);
+                        assertEquals(dest.getCoordPosition().y, armCoord.y, 0.5); 
+                    }
+                    catch(OutOfMemoryError e) {
+                        throw new AssertionError("Start: " + state.getClass().getName() + ", Dest: " + dest.getClass().getName());
+                    }
+                    double expectedIntakeAngle;
+                    if(dest.isIntakeDeployed()) {
+                        expectedIntakeAngle = Intake.INTAKE_ANGLE_DEPLOYED;
+                    } else {
+                        expectedIntakeAngle = Intake.INTAKE_ANGLE_IN;
+                    }
+                    assertEquals(true, MathUtils.isWithinTolerance(expectedIntakeAngle, intake.getAngleInDegrees(), 0.5));
                 }
-        
-                Point2D.Double armCoord = Arm.getCoordPosition(elbow.getAngleInDegrees(), wrist.getAngleInDegrees(), extension.getLengthInches());
-                assertEquals(dest, Arm.getCurrentState());
-                try {
-                    // System.out.println("start: " + state.getClass().getName() + ", dest: " + dest.getClass().getName());
-                    assertEquals(dest.getCoordPosition().x, armCoord.x, 0.5);
-                    assertEquals(dest.getCoordPosition().y, armCoord.y, 0.5); 
-                }
-                catch(OutOfMemoryError e) {
-                    throw new AssertionError("Start: " + state.getClass().getName() + ", Dest: " + dest.getClass().getName());
-                }
-                double expectedIntakeAngle;
-                if(dest.isIntakeDeployed()) {
-                    expectedIntakeAngle = Intake.INTAKE_ANGLE_DEPLOYED;
-                } else {
-                    expectedIntakeAngle = Intake.INTAKE_ANGLE_IN;
-                }
-                assertEquals(true, MathUtils.isWithinTolerance(expectedIntakeAngle, intake.getAngleInDegrees(), 0.5));
             }
         }
     }
